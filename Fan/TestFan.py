dc5c812e735582e0e15274e3c0e65ebffeacd84a
LeoSuave25 <suaverdez90@gmail.com>
2023-07-01 12:31:42 +0800

    Import Heading and the User_interaction class Done

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -2,3 +2,14 @@
 Assign medium speed, radius 5, color blue, and turn it off for the second object. 
 Display each object’s speed, radius, color, and on properties.'''
 from functions import Fan
+from style import Styling
+from user_interactions import UserInteraction
+headers = Styling()
+
+first_fan = Fan(Fan.FAST,True,10,"Yellow")
+second_fan = Fan(Fan.MEDIUM,False,5,"Blue")
+
+fans = [first_fan, second_fan]
+
+interaction = UserInteraction(fans)
+interaction.fan_interaction()
