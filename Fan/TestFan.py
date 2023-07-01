a41728237d482167ebee1d128a71049152504700
LeoSuave25 <suaverdez90@gmail.com>
2023-07-01 11:30:05 +0800

    Constructor done with default values and Constants done

---
+++ Fan/TestFan.py
@@ -0,0 +1,4 @@
+'''For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
+Assign medium speed, radius 5, color blue, and turn it off for the second object. 
+Display each object’s speed, radius, color, and on properties.'''
+from functions import Fan
---
+++ Fan/functions.py
@@ -0,0 +1,12 @@
+'''A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed
+'''
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+    def __init__(self,speed=0,power=False,radius=5,color="blue"):
+        self.__speed = speed
+        self.__power = power
+        self.__radius = float(radius)
+        self.__color = color
