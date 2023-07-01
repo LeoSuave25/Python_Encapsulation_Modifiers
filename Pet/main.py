39b8cf67da1433b7110bb858546d685a5b181086
LeoSuave25 <suaverdez90@gmail.com>
2023-07-01 21:06:15 +0800

    Compiled all classes to complete the program

---
+++ Pet/main.py
@@ -0,0 +1,8 @@
+from userinteract import UserInteraction
+from style import Styling
+
+headers = Styling()
+
+ui = UserInteraction()
+# Use the UserInteraction object to prompt the user and display the pet's info
+ui.retry()
